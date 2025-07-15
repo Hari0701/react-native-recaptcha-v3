17c97fa00cecd817c9fce5e1b31c08e7413934af
Hariharan A T <hari07.at@gmail.com>
2025-07-15 11:35:12 +0530

    fix: added getToken method

--- index.tsx
+++ index.tsx
@@ -1,36 +1,50 @@
-import * as React from 'react'
+import * as React from "react";
 import ReCaptchaComponent from "./src/ReCaptchaComponent";
 
 export type IProps = {
-  captchaDomain: string
-  onReceiveToken: (captchaToken: string) => void
-  siteKey: string
-  action: string
-}
+  captchaDomain: string;
+  onReceiveToken?: (captchaToken: string) => void;
+  siteKey: string;
+  action: string;
+};
 
-export type IState = {}
+class ReCaptchaV3 extends React.PureComponent<IProps> {
+  private _captchaRef = React.createRef<ReCaptchaComponent>();
+  private _tokenResolver: ((token: string) => void) | null = null;
 
-
-class ReCaptchaV3 extends React.PureComponent<IProps, IState> {
-
-  private _captchaRef: any
-
-  public refreshToken = () => {
-    this._captchaRef.refreshToken()
+  /**
+   * Triggers the CAPTCHA and returns the token when available.
+   */
+  public getToken(): Promise<string> {
+    return new Promise((resolve) => {
+      this._tokenResolver = resolve;
+      this._captchaRef.current?.refreshToken();
+    });
   }
+
+  private handleToken = (token: string) => {
+    if (this._tokenResolver) {
+      this._tokenResolver(token);
+      this._tokenResolver = null;
+    }
+
+    // Optional callback for consumer
+    if (this.props.onReceiveToken) {
+      this.props.onReceiveToken(token);
+    }
+  };
 
   render() {
     return (
       <ReCaptchaComponent
-        ref={ref => this._captchaRef = ref}
+        ref={this._captchaRef}
         action={this.props.action}
         captchaDomain={this.props.captchaDomain}
         siteKey={this.props.siteKey}
-        onReceiveToken={(token: string) => {
-          this.props.onReceiveToken(token)
-        }}/>
-    )
+        onReceiveToken={this.handleToken}
+      />
+    );
   }
 }
 
-export default ReCaptchaV3
+export default ReCaptchaV3;
